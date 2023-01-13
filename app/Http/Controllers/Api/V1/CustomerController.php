dfed1c2194a2599f87deb39cfa7e6be8c40f426d
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-01-13 09:31:22 +0200

    updating with put and patch

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use Illuminate\Http\Request;
 use App\Models\Customer;
 use App\Http\Controllers\Controller;
@@ -71,7 +71,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
     }
 
     /**
